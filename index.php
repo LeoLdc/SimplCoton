ba9715eef5d1b0893914caeedffc68a83738fb83
Leo <ledocq.leo@laposte.net>
2018-05-30 12:33:03 +0200

    maj

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+  <html>
+
+    <head>
+
+      <meta charset="utf-8">
+
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+
+      <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+      <title>SimplCoton</title>
+      <style>
+      .container{
+        width:199px;
+        height:148px;
+        position:relative;
+      }
+      .container img {
+        position:absolute;
+
+      }
+      .container.desc {
+        width:199px;
+        height:148px;
+        background-color: #000;
+        position: absolute;
+        color: white;
+        opacity:0.8;
+        display:none;
+      }
+      </style>
+    </head>
+
+    <STYLE>A {text-decoration: none;} </STYLE>
+
+    <body>
+
+      <h1 style="color:#303030;">Simplon</h1>
+
+      <br/>
+
+      <p>"Lorem upsyr hrfer jqedjoen,bfygfc heecb"</p>
+
+      <div id="container">
+
+        <div class="flexco">
+
+          <div class="container">
+
+            <div class="image">
+
+              <img src="images/simplon.jpg" alt="Simplon">
+              <div class="desc">
+
+                <p>Simplon</p>
+
+              </div>
+
+            </div>
+
+         </div>
+
+         <a title="lienun" href="https://simplon.co/">
+         Cliquez ici</a>
+
+       </div>
+
+      <div class="flexco">
+
+        <div class="container">
+
+          <div class="image">
+
+            <img src="images/presentation.jpg" alt="Présentation">
+
+            <div class="desc">
+
+              <p>Présentation de Simplon</p>
+
+            </div>
+
+          </div>
+
+         </div>
+
+         <a title="liendeux" href="https://simplon.co/">
+       Cliquez ici</a>
+
+
+
+      </div>
+
+    </div>
+
+    </body>
+
+    <div class="footer">
+
+      <h3 style="color:white;" id="nom">LEDOCQ Léo</h3>
+
+    </div>
+
+  </html>
